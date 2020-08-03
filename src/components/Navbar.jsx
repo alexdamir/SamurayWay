26171a03d20b6132d9afb4081362fa9d96b5f7c9
Aleksandr Malyi <aamalyy@gmail.com>
2020-08-03 19:44:11 -0400

    13 Add to the repository

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <nav className='nav'>
+        <div>Profile</div>
+        <div>Messages</div>
+        <div>News</div>
+        <div>Music</div>
+        <div>Settings</div>
+
+      </nav>
+
+    );
+}
+
+export default Navbar;

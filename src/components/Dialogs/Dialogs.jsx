508880b724c543d8ebc199033092fd9afc7e260a
Aleksandr Malyi <aamalyy@gmail.com>
2020-08-07 04:33:10 -0400

    23.

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,33 +2,40 @@ import React from 'react';
 import css from './Dialog.module.css';
 import { NavLink } from 'react-router-dom';
 
+const DialogItem = (props) => {
+    let path = '/dialogs/' + props.id;
+    return (
+        <div className={css.dialog}>
+            <NavLink to={path} activeClassName={css.active}>{props.name}</NavLink>
+        </div>
+    );
+}
+
+const Message = (props) => {
+    return (
+        <div className={css.message}>{props.message}</div>
+    );
+}
+
+
 const Dialogs = (props) => {
     return (
         <div className={css.dialogs}>
             <div className={css.dialogItems}>
-                <div className={css.dialog}>
-                    <NavLink to='/dialogs/1'>Dimych</NavLink>
-                </div>
-                <div className={css.dialog}>
-                    <NavLink to='/dialogs/2'>Andrey</NavLink>
-                </div>
-                <div className={css.dialog}>
-                    <NavLink to='/dialogs/3'>Sveta</NavLink>
-                </div>
-                <div className={css.dialog}>
-                    <NavLink to='/dialogs/4'>Sasha</NavLink>
-                </div>
-                <div className={css.dialog}>
-                    <NavLink to='/dialogs/5'>Victor</NavLink>
-                </div>
-                <div className={css.dialog}>
-                    <NavLink to='/dialogs/6'>Valera</NavLink>
-                </div>
-           </div>
+                <DialogItem name="Dimych" id="1" />
+                <DialogItem name="Andrey" id="2" />
+                <DialogItem name="Sveta" id="3" />
+                <DialogItem name="Sasha" id="4" />
+                <DialogItem name="Victor" id="5" />
+                <DialogItem name="Valera" id="6" />
+
+            </div>
             <div className={css.messages}>
-                <div className={css.message}>Hi</div>
-                <div className={css.message}>How is your it-kamasutra?</div>
-                <div className={css.message}>Yo</div>
+                <Message message="Hi" />
+                <Message message="How is your it-kamasutra?" />
+                <Message message="Yo" />
+                <Message message="Yo" />
+                <Message message="Yo" />
             </div>
         </div>
     )

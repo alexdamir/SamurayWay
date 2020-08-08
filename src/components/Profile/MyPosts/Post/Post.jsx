9d8134fc2fb29b41527162c7aef30bccaf4ef064
Aleksandr Malyi <aamalyy@gmail.com>
2020-08-08 02:21:14 -0400

    33. kortimki negruzyatsya

--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -5,7 +5,7 @@ const Post = (props) => {
     return (
 
         <div className={`${css.item} ${css.active}`}>
-            <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTq4H7mPnTLV9rtqZ7_W5NeAhudM0OXP3UNNQ&usqp=CAU' alt='' />
+            <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTq4H7mPnTLV9rtqZ7_W5NeAhudM0OXP3UNNQ&usqp=CAU' alt='avatar' />
                 {props.message}
                 <div>
                     <span>Like</span> {props.likescount}
